89392b22ab02b324bed23074044d009cf1eb41d0
Abdullah Shaikh <abdullahshaikh88147@gmail.com>
2024-04-07 16:36:18 +0530

    Added role model and define association between role and user model through user_roles model

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser=require('body-parser');
 const { PORT }=require('./config/serverConfig');
 const apiRoutes=require('./routes/index');
 
+const db=require('./models/index');
+const { User,Role }=require('./models/index');
+const res = require('express/lib/response');
 
 const app=express();
 
@@ -12,7 +15,16 @@ const prepareAndStart=async()=>{
     app.use('/api',apiRoutes);
     app.listen(PORT,async()=>{
         console.log(`Server Started at : ${PORT}`);
-        
+        if(process.env.DB_SYNC){
+            db.sequelize.sync();
+        }
+
+        const u1=await User.findByPk(1);
+        const r1=await Role.findByPk(2);
+        // u1.addRole(r1);
+        // console.log(process.env.DB_SYNC)
+        const response=await r1.getUsers();
+        console.log(response);
     })
 }
 
